f7005b506ac649d20dbfe9b79bbd7a1405f4ea9f
Mradul Agrawal <mradulagrawal126@gmail.com>
2022-08-07 12:43:16 +0530

    Accordian Created

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Accordian from './components/Accordian'
 
-function App() {
+const items =[
+{
+  title:'What is react?',
+  content:'React is a frontend javascript framework'
+},
+{
+  title:'Why use React?',
+  content:'React is a favourite Js library among Engineers'  
+},
+{
+  title:'How do you use React?',
+  content:'you use react by creating components'
+}
+]
+
+
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <Accordian items={items}/>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
