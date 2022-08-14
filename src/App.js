a0d73e27d52bea277fb9db9af18d26c7b4834124
Mradul Agrawal <mradulagrawal126@gmail.com>
2022-08-14 19:11:50 +0530

    Widgets Completed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
-import React from 'react'
+import React,{useState} from 'react'
 import Accordian from './components/Accordian'
+import Search from './components/Search'
+import Dropdown from "./components/Dropdown";
+import Translate from './components/Translate';
+import Route from './components/Route';
+import Header from './components/Header';
+
 
 const items =[
 {
@@ -16,14 +22,54 @@ const items =[
 }
 ]
 
+const options=[
+  {
+    label:'The color Red',
+    value:'red'
+  },
+  {
+    label:'The color Green',
+    value:'green'
+  },
+  {
+    label:'A shade of Blue',
+    value:'blue'
+  }
+]
+
 
 
 const App = () => {
-  return (
-    <div>
-        <Accordian items={items}/>
-    </div>
-  )
+  const [selected,setSelected] = useState(options[0])
+
+
+
+return(
+  <div>
+    <Header/>
+    <Route path="/">
+      <Accordian items={items}/>
+    </Route>
+
+    <Route path="/list">
+      <Search/>
+    </Route>
+
+    <Route path="/dropdown">
+      <Dropdown
+      label="Select a Color"
+      options={options}
+      selected={selected}
+      onSelectedChange={setSelected}
+      />
+    </Route>
+
+    <Route path="/translate">
+      <Translate/>
+    </Route>
+
+  </div>
+)
 }
 
 export default App
